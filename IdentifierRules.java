d30816121cb291e6a8049d78ae0947dff013cf8a
Abdusuper <abdulazizsaloxiddinov404@gmail.com>
2025-09-07 08:09:05 -0700

    Add files via upload

---
+++ IdentifierRules.java
@@ -0,0 +1,26 @@
+/**********************************************************************
+  
+  Program Title : Demonstrating Identifier Rules in Java
+  File Name: "IdentifierRules.java"
+  
+  Programmer : Abdulaziz Salokhiddinov
+  Contact Details : abdulazizsaloxiddinov404@gmail.com
+  Date : 04 September 2025
+  
+ **********************************************************************/
+
+public class IdentifierRules {
+    public static void main(String[] args) {
+        int secondPlace = 2;
+        int firstName = 10;
+        int price = 99;
+        int userName = 25;
+
+        System.out.println("Second Place: " + secondPlace);
+        System.out.println("First Name: " + firstName);
+        System.out.println("Price: " + price);
+        System.out.println("User Name: " + userName);
+
+        System.out.println("This program executes successfully after fixing invalid identifiers.");
+    }
+}
